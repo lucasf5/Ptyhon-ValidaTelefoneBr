88f74eaacc54478ebc11b1e667215e4d03366290
Lucas Ferreira <lucasfpnt@gmail.com>
2021-12-08 22:12:32 -0300

    first commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class TelefonesBr:
         else:
             raise ValueError('Numero incorreto')
     def valida_telefone(self, telefone):
-        padrao = '[0-9]{2,3}[0-9]{2}[0-9]{4,5}[0-9]{4}'
+        padrao = '([0-9]{2,3})?[0-9]{2}[0-9]{4,5}[0-9]{4}'
         resposta = (re.findall(padrao, telefone))
         if resposta:
             return True
@@ -17,7 +17,9 @@ class TelefonesBr:
             return False
 
     def formatarNumero(self, numero):
-        if len(numero) == 13:
+        if len(numero) == 11:
+            print(f'+55 {self.numero[0:2]} {self.numero[2]}-{self.numero[3:7]}-{self.numero[7:]}')
+        elif len(numero) == 13:
             print(f'+{self.numero[:2]} {self.numero[2:4]} {self.numero[4]}-{self.numero[5:9]}-{self.numero[9:]}')
         elif len(numero) == 14:
             print(f'+{self.numero[:3]} {self.numero[3:5]} {self.numero[5]}-{self.numero[6:10]}-{self.numero[10:]}')
@@ -26,4 +28,4 @@ class TelefonesBr:
 
 
 
-meu = TelefonesBr('55585982062167')
+meu = TelefonesBr('85982062167')
